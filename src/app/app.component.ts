bef2bf156d66f234fc36fad7908f9d4a9072862c
mamadou cire GUISSE <mamadou.cire.guisse@kyndryl.com>
2022-09-23 11:39:42 +0200

    update loadjson file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import * as fileSaver from 'file-saver';
 
 import { FormBuilder } from '@angular/forms';
 import { BackendService } from './@core/backend.service';
 
 var type = ["fraud", "fraud2", "non-fraud"];
+
 
 @Component({
   selector: 'app-root',
@@ -12,7 +15,11 @@ var type = ["fraud", "fraud2", "non-fraud"];
 })
 export class AppComponent {
   title = 'live fraud detector dashboard';
-  constructor(private backendService: BackendService, private formBuilder: FormBuilder) { }
+
+  name = 'data';
+  fileUrl: any;
+
+  constructor(private backendService: BackendService, private formBuilder: FormBuilder, private sanitizer: DomSanitizer) { }
   onSubmitForm = this.formBuilder.group({
     type: ''
   });
@@ -20,18 +27,27 @@ export class AppComponent {
 
   }
 
-  onSubmit(){
+  onSubmit() {
     this.onSubmitForm
     this.onSubmitForm.value.type = type[Math.floor(Math.random() * type.length)];
-    this.backendService.onSubmitService(this.onSubmitForm.value.type).subscribe(
-      (data)=>{
-        console.info('Your order has been submitted is succefully :', data.message);
+    this.backendService.onSubmitService(this.onSubmitForm.value.type).subscribe
+      (
+        (result: any) => {
+          console.info('Your order has been submitted is succefully :', result);
+          if (result.code === 210) {
+            console.info(result);
+            let blob: any = new Blob([JSON.stringify(result, null, 2)], { type: 'text/json; charset=utf-8' });
+            const url = window.URL.createObjectURL(blob);
+            fileSaver.saveAs(blob,'data.json');
+          }
+        },
+        (err) => {
+          console.warn('Error your order has been submitted', err);
+        }
+      )
+  }
 
-      },
-      (err)=>{
-        console.warn('Error your order has been submitted', err);
-      }
-    )
+  downloadJson (){
 
   }
 
